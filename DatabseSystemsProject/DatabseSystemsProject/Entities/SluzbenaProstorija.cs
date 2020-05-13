bce873a6210155c3fca8755f10062025695cc238
cakicpredragdeveloper <cakicpredrag@gmail.com>
2020-05-13 14:14:10 +0200

    add Entity SluzbenaProstorija & Map SluzbenaProstorijMap

---
+++ DatabseSystemsProject/DatabseSystemsProject/Entities/SluzbenaProstorija.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabseSystemsProject.Entities
+{
+    class SluzbenaProstorija
+    {
+        public int Id { get; set; }
+        public int Sprat { get; set; }
+        public int Broj { get; set; }
+
+        public SluzbenaProstorija()
+        {
+
+        }
+    }
+}
